63e798a916a217ef7579ac3fb64994c04a7b3d09
Abraham <abrahamph16@gmail.com>
2020-09-26 19:46:08 -0500

    First Project with Vue

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,36 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <main>
+    <px-header :links="links" />
+    <router-view class="container px-5 sm:px-20 py-20 flex justify-center " />
+    <px-footer />
+  </main>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import PxHeader from '@/components/PxHeader.vue';
+import PxFooter from '@/components/PxFooter';
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: { PxHeader, PxFooter },
+  data() {
+    return {
+      links: [
+        {
+          title: 'BTC',
+          to: { name: 'coin-detail', params: { id: 'bitcoin' } }
+        },
+        {
+          title: 'ETH',
+          to: { name: 'coin-detail', params: { id: 'ethereum' } }
+        },
+        {
+          title: 'USDT',
+          to: { name: 'coin-detail', params: { id: 'tether' } }
+        }
+      ]
+    };
   }
-}
+};
 </script>
 
 <style>
@@ -24,5 +41,8 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+body {
+  oveflow-x: hidden;
 }
 </style>
---
+++ src/components/PxFooter.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="attribution">
+    Coded by
+    <a
+      href="https://abrahamph-ny5dnzva6.vercel.app/"
+      class="developer"
+      target="_blank"
+    >
+      Abraham PH 🤘🏽</a
+    >.
+  </div>
+</template>
+
+<style scoped>
+.attribution {
+  text-align: center;
+  height: 50px;
+  background-color: #68d391;
+  color: #fff;
+}
+.developer {
+  color: blue;
+}
+</style>
